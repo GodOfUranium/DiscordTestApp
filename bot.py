00187a0fcebb849224ffcc02aacca3c0107fdfae
GodOfUranium <godofuranium@gmail.com>
2024-11-08 09:36:00 +0100

    added types unflip and smile to fun_command with error message for invalid type

--- bot.py
+++ bot.py
@@ -11,6 +11,9 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+def sendmsg(interaction, text:str):
+    interaction.response.send_message(text)
+
 @tree.command(
     name = "hello",
     description="Say Hello!"
@@ -25,6 +28,12 @@ async def hello_command(interaction):
 async def fun_command(interaction, type:str):
     if(type == "tableflip"):
         await interaction.response.send_message("(╯°□°)╯︵ ┻━┻")
+    elif(type == "unflip"):
+        await interaction.response.send_message("┬─┬ノ( º _ ºノ)")
+    elif(type == "smile"):
+        await interaction.response.send_message("(°u°)")
+    else:
+        await interaction.response.send_message("```An Error occured! Invalid input for type```")
 
 @client.event
 async def on_ready():
